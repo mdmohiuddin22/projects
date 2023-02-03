b77a8994182a8c5c0da782846f4bef9296489c73
Md.Mohi Uddin <saddammd203@gmail.com>
2023-02-03 07:43:49 +0600

    added all

--- apply-js-core-concepts/isEven.js
+++ apply-js-core-concepts/isEven.js
@@ -9,7 +9,15 @@
 
 function isEven(number){
     const remainder = number % 2;
-    console.log(remainder);
+  if(remainder === 0){
+    return true;
+  }
+  else{
+    return false;
+  }
 }
 
-isEven(48);
+const myNumberIsEven = isEven(303);
+console.log(myNumberIsEven);
+const herNumberIsEven = isEven(1280);
+console.log(herNumberIsEven);
---
+++ apply-js-core-concepts/isLeapYear.js
@@ -0,0 +1,24 @@
+// function isLeapYear(year){
+//     const remainder = year % 4;
+//     if(remainder === 0){
+//     return true;
+//     }
+//     else{
+//         return false;
+//     }
+// }
+function isLeapYear(year){
+    const remainder = year % 4;
+    if(remainder === 0){
+    return true;
+    }
+    else{
+        return false;
+    }
+}
+
+const isMyYearLeapYear= isLeapYear(1933);
+console.log('my year:',isMyYearLeapYear);
+
+const isHerYearLeapYear = isLeapYear(1960);
+console.log('Her year:',isHerYearLeapYear);
