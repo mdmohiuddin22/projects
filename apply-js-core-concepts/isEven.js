a061480cf540daab022ef11401ad519a6b506254
Md.Mohi Uddin <saddammd203@gmail.com>
2023-02-03 06:30:21 +0600

    added apply-js-core-concepts

---
+++ apply-js-core-concepts/isEven.js
@@ -0,0 +1,15 @@
+// console.log(4%2);
+// console.log(98%2);
+// console.log(144%2);
+// console.log(1286%2);
+// console.log(7%2);
+// console.log(99%2);
+// console.log(141%2);
+// console.log(1285%2);
+
+function isEven(number){
+    const remainder = number % 2;
+    console.log(remainder);
+}
+
+isEven(48);
---
+++ apply-js-core-concepts/milesTokm.js
@@ -0,0 +1,4 @@
+function milesTokilometer(miles){
+    const kilometer = miles*1.609;
+    return kilometer;
+}
